5d97290e0b0f86b2be018b3094171e63dbe03fee
Guilherme Baptista <guisgb13@gmail.com>
2015-08-26 16:38:38 -0300

    Setting default values for mode and android:inputType programatically so they don't break the marquee effect

--- app/src/main/java/com/guilhermesgb/shiftableedittext/view/MainActivity.java
+++ app/src/main/java/com/guilhermesgb/shiftableedittext/view/MainActivity.java
@@ -1,11 +1,11 @@
 package com.guilhermesgb.shiftableedittext.view;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.guilhermesgb.shiftableedittext.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
